446a2475067eeb14e30d178b50b3ef8409c537e5
Satish152 <rongalasatish15@gmail.com>
2021-07-17 18:11:09 +0530

    Updated to maven project 17-07-2021

--- src/testingDemo/TestDemo.java
+++ src/testingDemo/TestDemo.java
@@ -3,17 +3,30 @@ package testingDemo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestDemo {
 
+	@Parameters("Browser")
 	@Test
-	public void sampleTest(){
+	public void sampleTest(String browserName){
 		WebDriver driver=null;
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
+		if(browserName.equalsIgnoreCase("Chrome")){
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}else if(browserName.equalsIgnoreCase("Edge")){
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}else if(browserName.equalsIgnoreCase("firefox")){
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		
 		driver.manage().window().maximize();
 		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("test");
